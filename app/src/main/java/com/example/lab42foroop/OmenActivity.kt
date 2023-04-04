3adb123b3bdd2219448345bcf58f9eeba982249b
SVlad19 <mr.vladik.semeno3@gmail.com>
2023-04-04 10:06:20 +0300

    Menu

---
+++ app/src/main/java/com/example/lab42foroop/OmenActivity.kt
@@ -0,0 +1,11 @@
+package com.example.lab42foroop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OmenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_omen)
+    }
+}
