aa67a3e3f6e5ba408e304d417b3aa61a6a2e7e63
SVlad19 <mr.vladik.semeno3@gmail.com>
2023-04-10 18:24:26 +0300

    Activities without logic

---
+++ app/src/main/java/com/example/lab42foroop/CitationsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.lab42foroop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CitationsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_citations)
+    }
+}
---
+++ app/src/main/java/com/example/lab42foroop/ImageOfSeaActivity.kt
@@ -0,0 +1,11 @@
+package com.example.lab42foroop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ImageOfSeaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image_of_sea)
+    }
+}
